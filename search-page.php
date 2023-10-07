48677fead71b79feebdd14b832bfd3f9fe3dfaa0
Mandeep Bal <mbal099@mtroyal.ca>
2023-10-06 19:34:14 -0600

    started basic code for two of the files and started working wiht the database

---
+++ search-page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Basic Song Search</h1>
+
+    <form action = "results-page.php" metod = "get">
+        <label for = "songTitle">Title</label>
+        <input type = "text" id = "songTitle" name = "songTitle">
+        </br>
+        <label for = "artistName">Artist</label>
+        <input type = "text" id = "artistName" name = "artistName">
+        </br>
+        <label for = "genreName">Genre</label>
+        <input type = "text" id = "genretName" name = "genreName">
+        </br>
+        <label for = "year">Year</label>
+        <input type = "text" id = "year" name = "year">
+        </br>
+        <input type = "submit" value = "Search">
+    </form>
+</body>
+</html>
--- single-song-page.php
+++ single-song-page.php
@@ -1,45 +1,30 @@
+
 <?php require_once('config.inc.php'); ?>
-<!DOCTYPE html>
-<html>
-<body>
-<h1>Database Tester (PDO)</h1>
 <?php
+//tables: songs, artists, genres and types
+//title, genre_name, artist_name
     try {
         $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "select * from songs";
+        $sql = "
+            SELECT * FROM songs JOIN genres USING(genre_id) JOIN artists USING(artist_id)
+        ";
         $result = $pdo->query($sql);
         $data = $result->fetchAll(PDO::FETCH_ASSOC);
-        // loop through the data
-        // while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        //     echo $row['ArtistID'] . " - " . $row['LastName'] . "<br/>";
-        // }
+        $pdo = null;
 
         foreach($data as $row){
-            //can still use artistID here if you want to
-            echo $row['title'] . "<br/>";
+            echo "Title: " . $row['title'] . " Name: " . $row['artist_name'] . " Genre: " . $row['genre_name'] . " other info: " . $row['bpm'] . "</br>";
         }
-
-        /*
-
-        echo '<h2>-------- Second Loop --------</h2>';
-        //this wont do anything since the previous loop has already run through it all
-        //this was when the previous example was result as data
-        // foreach ($result as $row) {
-        //     echo $row['ArtistID'] . " - " . $row['LastName'] . "<br/>";
-        // }
-        foreach ($data as $row) {
-            echo '<option value="' . $row['ArtistID'] . '">';
-            echo $row['LastName'];
-            echo "</option>";
-            //echo $row['ArtistID'] . " - " . $row['LastName'] . "<br/>";
-        }*/
-        $pdo = null;
     }
     catch (PDOException $e) {
         die( $e->getMessage() );
     }
 ?>
+<!DOCTYPE html>
+<html>
+<body>
 </body>
 </html>
+
 
