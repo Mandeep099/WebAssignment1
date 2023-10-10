ff47c3f6f054d633bc23340342e30e0a0dc29744
Mandeep Bal <mbal099@mtroyal.ca>
2023-10-09 19:07:44 -0600

    started to implement the querry string that was required for the page, same error is occur as with results-page listed in the commit for that

--- single-song-page.php
+++ single-song-page.php
@@ -10,10 +10,11 @@
         $result = $pdo->query($sql);
         $data = $result->fetchAll(PDO::FETCH_ASSOC);
         $pdo = null;
-
+        /*
         foreach($data as $row){
             echo "Title: " . $row['title'] . " Name: " . $row['artist_name'] . " Genre: " . $row['genre_name'] . " other info: " . $row['bpm'] . "</br>";
-        }
+        }*/
+         echo $_GET['hiddenInput'];
     }
     catch (PDOException $e) {
         die( $e->getMessage() );
