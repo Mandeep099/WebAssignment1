a71d7582c570f7d5bfc00a257297a5ceb67a7b83
Mandeep Bal <mbal099@mtroyal.ca>
2023-10-10 15:23:38 -0600

    made this the default config folder

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,8 +1,5 @@
 <?php
-/efine('DBHOST', 'localhost');
-define('DBNAME', 'art');
 define('DBUSER', 'root');
 define('DBPASS', '');
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
-//define('DBCONNSTRING','sqlite:./databases/art.db');
+define('DBCONNSTRING','sqlite:./database/music.db');
 ?>
