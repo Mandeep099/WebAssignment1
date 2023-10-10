e127028f53f63d8acf737c36a7ba19eb8b37f54c
Mandeep Bal <mbal099@mtroyal.ca>
2023-10-10 15:22:36 -0600

    removed extra config file

--- config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-//define('DBHOST', 'localhost');
-//define('DBNAME', 'art');
-define('DBUSER', 'root');
-define('DBPASS', '');
-//define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
-define('DBCONNSTRING','sqlite:./database/music.db');
-?>
