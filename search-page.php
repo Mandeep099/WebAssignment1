9c57c64d73468ee0d89887737d51a24969eee8ff
Mandeep Bal <mbal099@mtroyal.ca>
2023-10-13 17:15:44 -0600

    started to implement css into the serach page

--- search-page.php
+++ search-page.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
+<link rel = "stylesheet" href = "css/style.css"> 
 <html>
 <body>
-    <h1>Basic Song Search</h1>
-
-    <form action = "results-page.php" metod = "GET">
-        <label for = "songTitle">Title</label>
-        <input type = "text" id = "songTitle" name = "songTitle">
-        </br>
-        <label for = "artistName">Artist</label>
-        <input type = "text" id = "artistName" name = "artistName">
-        </br>
-        <label for = "genreName">Genre</label>
-        <input type = "text" id = "genretName" name = "genreName">
-        </br>
-        <label for = "year">Year</label>
-        <input type = "text" id = "year" name = "year">
-        </br>
-        <input type = "submit" value = "Search">
-    </form>
+    <div class = "searchForm">
+        <form action = "results-page.php" metod = "GET">
+            <fieldset class = serachFieldset>
+                <legend>Song Search</legend>
+                <label for = "songTitle">Title</label>
+                <input type = "text" id = "songTitle" name = "songTitle">
+                </br>
+                <label for = "artistName">Artist</label>
+                <input type = "text" id = "artistName" name = "artistName">
+                </br>
+                <label for = "genreName">Genre</label>
+                <input type = "text" id = "genretName" name = "genreName">
+                </br>
+                <label for = "year">Year</label>
+                <input type = "text" id = "year" name = "year">
+                </br>
+                <input type = "submit" value = "Search">
+            </fieldset>
+        </form>
+    </div>
 </body>
 </html>
